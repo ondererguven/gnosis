4bb04b57925a8b8ac4518e6773dc290f859a4017
Onder Erguven <ondererguven@gmail.com>
2019-06-18 16:03:36 +0200

    add account vc to storyboard

---
+++ Gnosis/Controller/AccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccountViewController.swift
+//  Gnosis
+//
+//  Created by Onder Erguven on 18.06.2019.
+//  Copyright © 2019 Onder Erguven. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
